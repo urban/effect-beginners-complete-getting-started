c3e3090a65199a5a21668805848f1cbe4c65acf8
Urban Faubion <urban.faubion@gmail.com>
2024-09-13 22:50:15 -0700

    Add schema

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
+import { Schema } from "@effect/schema";
 import { Data, Effect } from "effect";
+
+/** Schema definition **/
+class Pokemon extends Schema.Class<Pokemon>("Pokemon")({
+  id: Schema.Number,
+  order: Schema.Number,
+  name: Schema.String,
+  height: Schema.Number,
+  weight: Schema.Number,
+}) {}
+
 
 /** Errors **/
 class FetchError extends Data.TaggedError("FetchError")<{}> {}
@@ -17,13 +28,17 @@ const jsonResponse = (response: Response) =>
     catch: () => new JsonError(),
   });
 
+const decodePokemon = Schema.decodeUnknown(Pokemon);
+
 const program = Effect.gen(function* () {
   const response = yield* fetchRequest;
   if (!response.ok) {
     return yield* new FetchError();
   }
 
-  return yield* jsonResponse(response);
+  const json = yield* jsonResponse(response);
+
+  return yield* decodePokemon(json);
 });
 
 
@@ -32,8 +47,10 @@ const main = program.pipe(
   Effect.catchTags({
     FetchError: () => Effect.succeed("Fetch error"),
     JsonError: () => Effect.succeed("Json error"),
+    ParseError: () => Effect.succeed("Parse error"),
   })
 );
+
 
 /** Running effect **/
 Effect.runPromise(main).then(console.log);
