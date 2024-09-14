9e299cc0d7998d526a9782a5962ab193cf4d8511
Urban Faubion <urban.faubion@gmail.com>
2024-09-13 22:21:52 -0700

    Add error handling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
-import { Console, Effect } from "effect";
+import { Effect, Data } from "effect";
 
-const main = Console.log("Hello world");
+class FetchError extends Data.TaggedClass("FetchError")<{}> {}
+class JsonError extends Data.TaggedClass("JsonError")<{}> {}
 
-Effect.runSync(main);
+const fetchRequest = Effect.tryPromise({
+  try: () => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/"),
+  catch: () => new FetchError(),
+});
+
+const jsonResponse = (response: Response) =>
+  Effect.tryPromise({
+    try: () => response.json(),
+    catch: (): JsonError => new JsonError(),
+  });
+
+const main = fetchRequest.pipe(
+  Effect.filterOrFail(
+    (response) => response.ok,
+    () => new FetchError()
+  ),
+  Effect.flatMap(jsonResponse),
+  Effect.catchTags({
+    FetchError: () => Effect.succeed("Fetch error"),
+    JsonError: () => Effect.succeed("Json error"),
+  })
+);
+
+Effect.runPromise(main).then(console.log);
