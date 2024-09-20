86563f2731e207131bc6be51479e2a87fac14132
Urban Faubion <urban.faubion@gmail.com>
2024-09-19 17:50:20 -0700

    Add testing with services

---
+++ src/index.test.ts
@@ -0,0 +1,38 @@
+import { ConfigProvider, Effect, Layer } from "effect";
+import { afterAll, afterEach, beforeAll, expect, it } from "vitest";
+import { server } from "../test/node";
+import { PokeApi } from "./PokeApi";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+const TestConfigProvider = ConfigProvider.fromMap(
+  new Map([["BASE_URL", "http://localhost:3000"]])
+);
+
+const ConfigProviderLayer = Layer.setConfigProvider(TestConfigProvider);
+
+const program = Effect.gen(function* () {
+  const pokeApi = yield* PokeApi;
+  return yield* pokeApi.getPokemon;
+});
+
+const MainLayer = PokeApi.Live.pipe(
+  Layer.provide(ConfigProviderLayer),
+);
+
+const main = program.pipe(Effect.provide(MainLayer));
+
+it("returns a valid pokemon", async () => {
+  const response = await Effect.runPromise(main);
+
+  expect(response).toEqual({
+    id: 1,
+    height: 10,
+    weight: 10,
+    order: 1,
+    name: "myname"
+  });
+
+});
---
+++ test/handlers.ts
@@ -0,0 +1,17 @@
+import { HttpResponse, http } from 'msw';
+import type { Pokemon } from '../src/schemas';
+
+const mockPokemon: Pokemon = {
+  id: 1,
+  height: 10,
+  weight: 10,
+  order: 1,
+  name: "myname",
+};
+
+export const handlers = [
+  // 👇 Intercept requests for any pokemon (using wildcard `*`)
+  http.get("http://localhost:3000/api/v2/pokemon/*", () => {
+    return HttpResponse.json(mockPokemon);
+  }),
+];
